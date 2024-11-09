ec5bf20f82cca328a5fd3ecaf9a355b0de5c39b0
Ayush vashisth <ayushvashisth@Ayushs-MacBook-Air.local>
2024-11-09 22:02:55 +0530

    whatever

---
+++ Linked-list/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// g++ ./Linked-list/main.cpp -o ./Linked-list/main && ./Linked-list/main
+int main(){
+    cout<<"Hello world" << endl;
+
+    return 0;
+}
