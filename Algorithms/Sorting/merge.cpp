79a45301096dcb2dafb6c6e6099253238c95a3b2
Ayush vashisth <ayushvashisth@Blanche.local>
2025-04-15 21:45:25 +0530

    updated - 15/04/25

--- Algorithms/Sorting/merge.cpp
+++ Algorithms/Sorting/merge.cpp
@@ -4,8 +4,33 @@
 
 using namespace std;
 
+// g++ ./Algorithms/Sorting/merge.cpp -o ./Algorithms/Sorting/merge && ./Algorithms/Sorting/merge 
+
 void merge(int arr[],int low, int mid, int high){
-    
+    vector<int> temp;
+    int i = low;
+    int j = mid+1;
+    while(i <= mid && j <= high){
+        if(arr[i]<arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        if(arr[i]>=arr[j]){
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while(j<=high){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i] = temp[i-low];
+    }
 }
 
 void mergeSort(int arr[],int low,int high){
@@ -17,9 +42,10 @@ void mergeSort(int arr[],int low,int high){
 }
 
 int main(){
-    int arr[] = {3,1,4,2,8,11,19};
+    int arr[] = {1,3,1,4,2,8,11,0,19};
+            //   0 1 1 2 3 4 8 11 19
     int s = sizeof(arr)/sizeof(arr[0]);
-    mergesort(arr,0,s-1);
+    mergeSort(arr,0,s-1);
     for(int i =0;i<s;i++){
         cout << arr[i] << endl;
     }
